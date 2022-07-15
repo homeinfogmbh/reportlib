8a792953054b548261b9ac74d9c72d27ca29c814
Richard Neumann <r.neumann@homeinfo.de>
2022-07-15 11:05:11 +0200

    Add external interface

---
+++ reportlib/__init__.py
@@ -0,0 +1,22 @@
+"""Library for management of report of user-generated content."""
+
+from reportlib.functions import report_offer
+from reportlib.functions import report_response
+from reportlib.functions import report_topic
+from reportlib.functions import report_user_event
+from reportlib.orm import OfferReport
+from reportlib.orm import ResponseReport
+from reportlib.orm import TopicReport
+from reportlib.orm import UserEventReport
+
+
+__all__ = [
+    'OfferReport',
+    'ResponseReport',
+    'TopicReport',
+    'UserEventReport',
+    'report_offer',
+    'report_response',
+    'report_topic',
+    'report_user_event'
+]
